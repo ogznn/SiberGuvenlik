bd4eb8aef4cbc82b834d9f90ac0b52102a1029c6
ersinfurkan <oguzhanersoy55@gmail.com>
2022-04-22 16:41:43 +0300

    Siber Güvenlik
    Siber Güvenlik vize ödevi Oğuzhan Ersoy

---
+++ OğuzhanOdev/OğuzhanOdev/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace OğuzhanOdev
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        string ip;
+        Console.Write("IP Giriniz: ");
+        ip = Console.ReadLine();
+        //Burada ip değişkeni tanımlayarak kullanıcıdan ip girmesini istedik.
+
+        Process process = new Process();
+        process.StartInfo.FileName = "nmap.exe"; //Burada ise çalıştırılması gereken dosya adını sisteme belirttik.
+        process.StartInfo.Arguments = "nmap -T4 -A -v " + ip; //Devamında, çalıştırılan dosyaya girilmesi gereken metni belirtip, kullanıcının da girmiş olduğu ip adresini metin ile birleştirdik.
+        process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized; //Ek olarak sisteme programın başlatılması gereken bilgiyi verdik.
+        process.Start(); //Program başlatıldı.
+        process.WaitForExit(); //Program bitirilip çıkışı yapıldı.
+    }
+}
+}
